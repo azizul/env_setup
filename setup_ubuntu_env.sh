d4107ecb2b0a501a20dd26b1a8e9e85ee2d7896c
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2022-08-17 17:32:34 +0800

    try a few things

--- setup_ubuntu_env.sh
+++ setup_ubuntu_env.sh
@@ -6,15 +6,41 @@ echo $password | sudo -S add-apt-repository ppa:neovim-ppa/stable
 echo $password | sudo -S apt-get update -y
 echo $password | sudo -S apt-get install neovim -y
 
+# development dependencies
+$password | sudo -S apt-get apt install build-essential libreadline-dev unzip -y
+
 # programming tool set
 # 1.0 lua
-echo $password | sudo -S apt-get install lua5.3 -y
+curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
+tar -zxf lua-5.3.5.tar.gz
+cd lua-5.3.5
+make linux test
+$password | sudo -S make install
+
+# 1.1 Install lua package manager
+cd ..
+wget https://luarocks.org/releases/luarocks-3.8.0.tar.gz
+tar zxpf luarocks-3.8.0.tar.gz
+cd luarocks-3.8.0
+./configure --with-lua-include=/usr/local/include
+make
+echo $password | sudo -S make install
+
+# 1.2 cleanup
+cd ..
+rm -rf lua-5.3.5
+rm -rf lua-5.3.5.tar.gz
+rm -rf luarocks-3.8.0
+rm -rf luarocks-3.8.0.tar.gz
+
 # 2.0 nodejs via nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 # need to run manually the following
 # source ~/.bashrc
-# NVM='nvm install --lts'
-# eval $NVM
+# nvm install --lts
+
+# 3.0 Go
+echo $password | sudo -S apt-get install golang-go -y
 
 # neovim dependency
 
