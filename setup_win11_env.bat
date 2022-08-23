133c15ad141c2bee7b20d824a80f44ba9433671c
Azizul Rahman Bin Azizan <azizul80@gmail.com>
2022-08-23 15:47:32 +0800

    Add powershell config for win11

--- setup_win11_env.bat
+++ setup_win11_env.bat
@@ -27,6 +27,7 @@ choco install winmerge -y
 choco install wireshark -y
 choco install luarocks -y
 choco install ripgrep -y
+choco install lazygit -y
 
 # installing for latest version not in choco packages
 # TODO add manual download, unzip and system env setup
@@ -44,3 +45,24 @@ pip install black
 # npm packages
 npm i -g neovim
 npm i -g prettier
+
+
+# windows terminal customization
+# manually install powershell from windows store
+# 1.0 get powershell theme and customization
+winget install JanDeDobbeleer.OhMyPosh -s winget
+# 1.1 to update
+winget upgrade JanDeDobbeleer.OhMyPosh -s winget
+# 2.0 create path and file for the powershell config
+New-Item -Path $PROFILE -Type File -Force
+# add the following to the file
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
+# select the themes in $env:POSH_THEMES_PATH
+# Get the terminal shell icon from https://www.powershellgallery.com/packages/Terminal-Icons
+Install-Module -Name Terminal-Icons
+# for some odd reason can't delete the old version of PSReadLine
+Install-Module -Name PSReadLine -f
+# install Z
+Install-Module -Name z
+# copied the config from windows11/config/powershell/ to $PROFILE
+
